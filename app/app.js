08c2df38de18055bcdc771793e06d1e0f2bb5a34
Maria  Garcia <MariaGarcia@Marias-MacBook-Pro.local>
2017-03-06 01:53:02 -0600

    Not Finished

---
+++ app/app.js
@@ -0,0 +1,12 @@
+// Include the Main React Dependencies
+var React = require("react");
+var ReactDom = require("react-dom");
+
+// Requiring the Main Component
+var Main = require("./components/main");
+
+// Grabs the Routes
+var Routes = require("./config/routes");
+
+// Renders the contents according to the route page
+ReactDOM.render(routes, document.getElementById("app"));
